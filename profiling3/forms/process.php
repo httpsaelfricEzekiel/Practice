840d3bfbb4171af498655a72daccb097d7071868
aelfricEzekiel <ezekielnii123@gmail.com>
2024-03-06 16:17:58 +0800

    nag upload sa git

---
+++ profiling3/forms/process.php
@@ -0,0 +1,34 @@
+<?php
+    include "conn.php";
+    
+    if(isset($_POST['create_account'])){
+        $profile_pic = $_POST['pic'];
+        $lastName = $_POST['lastName'];
+        $firstName = $_POST['firstName'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $validate = mysqli_query($conn, "SELECT * FROM students WHERE email = '$email'");
+        $number = mysqli_num_rows($validate);
+
+        if($number <= 0){
+            $insert = mysqli_query($conn, "INSERT INTO students VALUES ('0', '$profile_pic', '$lastName', '$firstName', '$email', '$password')");
+
+            if($insert == true){
+                ?>
+                    <script>
+                        alert("data inserted");
+                        window.location.href="index.php";
+                    </script>
+                <?php
+            }
+        } else {
+            ?>
+                <script>
+                    alert("Email already Exists");
+                    window.location.href="index.php";
+                </script>
+            <?php
+        }
+    }
+?>
