aec734a62be392c55d4497af8211fef66bfa4f7f
aelfricEzekiel <ezekielnii123@gmail.com>
2024-04-23 19:37:00 -0700

    modified changes in validating a password

--- PHP_Practice/register.php
+++ PHP_Practice/register.php
@@ -6,8 +6,9 @@
         $last_name = test_input($_POST['lastName']);
         $email = test_input($_POST['email']);
         $password = test_input($_POST['pass']);
+        $c_password = test_input($_POST['pass']);
 
-        $firstNameErr = $lastNameErr = $emailErr = $passwordErr = " ";
+        $firstNameErr = $lastNameErr = $emailErr = $passwordErr = $c_passwordErr = "";
 
         if(isset($_POST['registerAccount'])){
             
@@ -28,6 +29,34 @@
 
                 if(!preg_match("/^[a-zA-Z-' ]*$/", $lastName)){
                     $lastNameErr = "Only letters and white spaces required!";
+                }
+            }
+
+            if(empty($email)){
+                $emailErr = "Email is required!";
+            } else {
+                $email_name = $email;
+
+                if(!filter_var($email_name, FILTER_VALIDATE_EMAIL)){
+                    
+                } else {
+                    $emailErr = "Invalid email format!";
+                }
+            }
+
+            if(empty($password) AND ($password == $c_password)){
+                $passwordErr = "Password is required!";
+            } else {
+                if(strlen($password) <= '8'){
+                    $passwordErr = "Your password must contain at least 8 characters!";
+                } else if(!preg_match("#[0-9]+#", $password)){
+                    $passwordErr = "Your password must contain at least 1 number!";
+                } else if (!preg_match("#[A-Z]+#", $password)){
+                    $passwordErr = "Your password must contain at least 1 upper case letter!";
+                } else if (!preg_match("#[a-z]+#", $password)){
+                    $passwordErr = "Your password must contain at least 1 lower case letter!";
+                } else {
+                    $c_passwordErr = "Check you've entered your or Confirm your password!";
                 }
             }
         } 
@@ -51,8 +80,8 @@
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         <input type="text" name="firstName" id="firstName" placeholder="Enter first name"> <span><?php echo $firstNameErr; ?></span> <br>
         <input type="text" name="lastName" id="lastName" placeholder="Enter last name"> <span><?php echo $lastNameErr; ?></span><br>
-        <input type="email" name="email" id="email" placeholder="Enter email"><br>
-        <input type="password" name="pass" id="pass" placeholder="Enter password"><br>
+        <input type="email" name="email" id="email" placeholder="Enter email"> <span><?php echo $emailErr; ?></span><br>
+        <input type="password" name="pass" id="pass" placeholder="Enter password"> <span><?php echo $passwordErr; ?></span><br>
         <a href="login.php">Login</a>
         <button type="submit" name="registerAccount">Register</button>
     </form>
