fcbe2785c2c8af7b6a276df9600702d6340b6b80
aelfricEzekiel <ezekielnii123@gmail.com>
2024-05-20 05:53:57 -0700

    create a toggle password

--- PHP_Practice/assets/js/script.js
+++ PHP_Practice/assets/js/script.js
@@ -25,3 +25,22 @@ const subForm = () => {
     });
 }
 subForm();
+
+const showPassword = () => {
+    document.querySelectorAll('.eye-show-pass').forEach(function(element, i){
+        element.addEventListener('click', function(e){
+            e.preventDefault()
+
+            if(document.querySelectorAll('.toggle-password')[i].getAttribute("type") == "password"){
+                document.querySelectorAll('.toggle-password')[i].setAttribute("type", "text")
+                element.classList.remove('fa-eye');
+                element.classList.add('fa-eye-slash');
+            } else {
+                document.querySelectorAll('.toggle-password')[i].setAttribute("type", "password")
+                element.classList.remove('fa-eye-slash');
+                element.classList.add("fa-eye")
+            }
+        })
+    })
+}
+showPassword();
