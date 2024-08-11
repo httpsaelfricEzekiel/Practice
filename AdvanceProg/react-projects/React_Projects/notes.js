67836003f62c46d85e9595fdd92108fc649ed151
aelfricEzekiel <ezekielnii123@gmail.com>
2024-08-11 03:35:00 -0700

    stick notes activity

---
+++ AdvanceProg/react-projects/React_Projects/notes.js
@@ -0,0 +1,49 @@
+
+class Notes extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            notes: [],
+            newNote: ""
+        };
+
+        this.handleInputChange = this.handleInputChange;
+        this.handleAddNote = this.handleAddNote;
+        this.handleClearNotes = this.handleClearNotes;
+        this.handleShowAllNotes = this.handleShowAllNotes;
+    }
+
+    handleInputChange(e){
+        this.setState({newNote: e.target.value});
+    }
+
+    handleAddNote(){
+        if(this.state.newNote.trim()){
+            this.setState(noteState => ({
+                notes: [...noteState.notes, noteState.newNote],
+                newNote: ""
+            }));
+        }
+    }
+    render(){
+        return (
+            <div>
+                <h1>Stick Notes</h1>
+                <input type="text" onChange={this.handleInputChange} value={this.state.newNote}/>
+                <button onClick={this.handleAddNote}>Add Notes</button>
+                <button>Clear Notes</button>
+                <button>Show all Notes</button>
+                <ul>
+                    {this.state.notes.map((note, index) => (
+                        <li key={index}>
+                            {note}
+                        </li>
+                    ))};
+                </ul>
+            </div>
+        )
+
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Notes/>);
