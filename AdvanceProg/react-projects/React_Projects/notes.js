fab8ea6af1a32e0cf9a2ce304ad7a8e147d4d44a
aelfricEzekiel <ezekielnii123@gmail.com>
2024-08-11 21:45:29 -0700

    added a activity notes

--- AdvanceProg/react-projects/React_Projects/notes.js
+++ AdvanceProg/react-projects/React_Projects/notes.js
@@ -12,30 +12,40 @@ class Notes extends React.Component {
         this.handleShowAllNotes = this.handleShowAllNotes;
     }
 
-    handleInputChange(e){
+    handleInputChange = (e) => {
         this.setState({newNote: e.target.value});
     }
 
-    handleAddNote(){
-        this.setState((note_state) => ({
-            notes: [...note_state.notes, note_state.newNote],
-            newNote: ''
-        }));
+    handleAddNote = () => {
+        if(this.state.newNote.trim()){
+            this.setState((note_state) => ({
+                notes: [...note_state.notes, note_state.newNote],
+                newNote: ""
+            }));
+        }
     }
+    handleClearNotes = () => {
+        this.setState({notes: []});
+    }
+
+    handleShowAllNotes = () => {
+        console.log(this.state.notes);
+    }
+
     render(){
         return (
             <div>
                 <h1>Stick Notes</h1>
                 <input type="text" onChange={this.handleInputChange} value={this.state.newNote}/>
                 <button onClick={this.handleAddNote}>Add Notes</button>
-                <button>Clear Notes</button>
-                <button>Show all Notes</button>
+                <button onClick={this.handleClearNotes}>Clear Notes</button>
+                <button onClick={this.handleShowAllNotes}>Show all Notes</button>
                 <ul>
                     {this.state.notes.map((note, index) => (
                         <li key={index}>
                             {note}
                         </li>
-                    ))};
+                    ))}
                 </ul>
             </div>
         )
