7ead1741d937a0564ce0c760489fcd7170b985ef
aelfricEzekiel <ezekielnii123@gmail.com>
2024-08-18 01:32:02 -0700

    modify current date in sticky notes

--- AdvanceProg/react-projects/React_Projects/notes.js
+++ AdvanceProg/react-projects/React_Projects/notes.js
@@ -1,8 +1,8 @@
 function Notes() {
     const [notes, setNotes] = React.useState([]);
-    
+
     const currDate = new Date();
-    const date = `${currDate.getMonth() + 1}/${currDate.getDate()}/${currDate.getFullYear()} ${currDate.getMinutes() - 1}`;
+    const date = `${currDate.getMonth() + 1}/${currDate.getDate()}/${currDate.getFullYear()} ${currDate.getHours() + 3}:${currDate.getMinutes()}:${currDate.getSeconds()}`;
 
     function handleInputChange(e){
         setNotes([e.target.value]);
@@ -16,13 +16,13 @@ function Notes() {
     }
 
     const handleShowAllNotes = () => {
-        alert(`\n${notes}\n${date}`);
+        alert(`\n${notes}\n${date}\n`);
     }
 
     return (
         <div>
             <h1>Stick Notes</h1>
-            <input type="text" onChange={handleInputChange} value={notes}/>
+            <input type="text" onInput={handleInputChange} value={notes}/>
             <button onClick={handleAddNote}>Add Notes</button>
             <button onClick={handleClearNotes}>Clear Notes</button>
             <button onClick={handleShowAllNotes}>Show all Notes</button>
