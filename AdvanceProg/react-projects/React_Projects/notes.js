afaf4e5d26cff551420dd4d5d91117d0a14941aa
aelfricEzekiel <ezekielnii123@gmail.com>
2024-08-18 21:27:16 -0700

    modified sticky notes

--- AdvanceProg/react-projects/React_Projects/notes.js
+++ AdvanceProg/react-projects/React_Projects/notes.js
@@ -1,14 +1,20 @@
 function Notes() {
     const [notes, setNotes] = React.useState([]);
-
+    
+    const newNote = {
+        addedNote: notes
+    }
+    
     const currDate = new Date();
-    const date = `${currDate.getMonth() + 1}/${currDate.getDate()}/${currDate.getFullYear()} ${currDate.toLocaleTimeString()}`;
+    const date = `${currDate.getMonth() + 1}/${currDate.getDate()}/${currDate.getFullYear()}, ${currDate.toLocaleTimeString()}`;
 
     function handleInputChange(e){
+        e.preventDefault();
         setNotes([e.target.value]);
     }
-    const handleAddNote = () => {   
-        setNotes([...notes, notes]);
+
+    const handleAddNote = (addedNote) => {   
+        setNotes([...notes, addedNote]);
     }
 
     const handleClearNotes = () => {
@@ -23,12 +29,13 @@ function Notes() {
         <div>
             <h1>Stick Notes</h1>
             <input type="text" onChange={handleInputChange} value={notes}/>
-            <button onClick={handleAddNote}>Add Notes</button>
+            <button onClick={() => handleAddNote(newNote.addedNote)}>Add Notes</button>
             <button onClick={handleClearNotes}>Clear Notes</button>
             <button onClick={handleShowAllNotes}>Show all Notes</button>
             {notes.map((note, index) => (
                 <p key={index}>
                     {`${note}`}
+                    <br/>
                     <br/>
                     {`${date}`}
                 </p>
