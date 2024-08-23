99ff5f065b1ba850ca9a174c8286ea4681cb5f66
aelfricEzekiel <ezekielnii123@gmail.com>
2024-08-22 22:59:57 -0700

    figure out the problem in sticky notes

--- AdvanceProg/react-projects/React_Projects/notes.js
+++ AdvanceProg/react-projects/React_Projects/notes.js
@@ -1,20 +1,29 @@
 function Notes() {
     const [notes, setNotes] = React.useState([]);
-    
-    const newNote = {
-        addedNote: notes
-    }
+    const [inputNotes, setInputNotes] = React.useState("");
     
     const currDate = new Date();
     const date = `${currDate.getMonth() + 1}/${currDate.getDate()}/${currDate.getFullYear()}, ${currDate.toLocaleTimeString()}`;
 
+    const newNote = {
+        addedNote: inputNotes,
+        date: date
+    }
+        
+
     function handleInputChange(e){
         e.preventDefault();
-        setNotes([e.target.value]);
+        setInputNotes(e.target.value);
     }
 
-    const handleAddNote = (addedNote) => {   
-        setNotes([...notes, addedNote]);
+    const handleAddNote = (addedNote) => {  
+        if(inputNotes.trim() != ""){
+            setNotes([addedNote, ...notes]);
+            setInputNotes("");
+            alert("Notes Added!");
+        } else {
+            alert("No Added Notes")
+        }
     }
 
     const handleClearNotes = () => {
@@ -22,20 +31,23 @@ function Notes() {
     }
 
     const handleShowAllNotes = () => {
-        alert(`\n${notes}\n${date}\n`);
+        if(notes.length){
+            alert(notes.map((n) => `${n}\n${newNote.date}\n`).join(""));
+        } else {
+            alert("Notes is empty!");
+        }
     }
 
     return (
         <div>
             <h1>Stick Notes</h1>
-            <input type="text" onChange={handleInputChange} value={notes}/>
+            <input type="text" onChange={handleInputChange} value={inputNotes}/>
             <button onClick={() => handleAddNote(newNote.addedNote)}>Add Notes</button>
             <button onClick={handleClearNotes}>Clear Notes</button>
             <button onClick={handleShowAllNotes}>Show all Notes</button>
             {notes.map((note, index) => (
                 <p key={index}>
                     {`${note}`}
-                    <br/>
                     <br/>
                     {`${date}`}
                 </p>
